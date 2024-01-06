49722263572429d225d22f07f00903bd7cb9a1e4
Ankit Bamal <ankitbamal99ab@gmail.com>
2024-01-06 12:27:30 +0530

    Using a static folder in project simple

--- Desktop/django_project/myproject/applications/members/urls.py
+++ Desktop/django_project/myproject/applications/members/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members'),
-    path('authorized/',views.authorized),
+    # path('members/', views.members, name='members'),  we are commenting this line because we are not using function for views , instead for this we are writing a new line for memberclass view
+    path('members/',views.MembersView.as_view()), # this is for class view
+    # path('authorized/',views.authorized),
+    path('authorized/',views.AuthorizedView.as_view()),
 ]
--- Desktop/django_project/myproject/applications/members/views.py
+++ Desktop/django_project/myproject/applications/members/views.py
@@ -4,9 +4,25 @@ from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def members(request):
-    return render(request,'members/welcome.html',{'today':datetime.today()})
+# def members(request):
+#     return render(request,'members/welcome.html',{'today':datetime.today()})
 
-@login_required(login_url ='/admin')
-def authorized(request):
-    return render(request,'members/authorized.html',{})
+# @login_required(login_url ='/admin')
+# def authorized(request):
+#     return render(request,'members/authorized.html',{})
+
+# insted of function we want to make a class for views for that we have to import
+# create a new class and we can comment the function members and create a new class MembersView
+from django.views.generic import TemplateView
+class MembersView(TemplateView):
+    template_name='members/welcome.html'  # this is for view
+    extra_context= {'today':datetime.today()} # this is for extra Variable
+    #  after add this new code we have to chnage in url also
+
+
+# for class auth
+from django.contrib.auth.mixins import LoginRequiredMixin
+class AuthorizedView(LoginRequiredMixin,TemplateView):
+    template_name= 'members/authorized.html'
+    login_url= '/admin'
+
